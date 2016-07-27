f4a6990881c91729b81c114b763fad8a1aa6631d
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2016-07-27 15:31:11 +0700

    ....

---
+++ HelloAndroid/app/src/main/java/helloandroid/minhvu/com/helloandroid/MainActivity.java
@@ -0,0 +1,17 @@
+package helloandroid.minhvu.com.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
